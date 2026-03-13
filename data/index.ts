35311584521be7cbb6bbdca202a697f499b29b7e
Juvaid <117333674+Juvaid@users.noreply.github.com>
2026-03-13 12:20:21 +0530

    Create updated file data/index.ts for central static content export

---
+++ data/index.ts
@@ -0,0 +1,6 @@
+// Central export point for all static content
+
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as Sidebar } from './Sidebar';
+// Add more exports as needed
