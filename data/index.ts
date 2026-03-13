d22b7d4dc5baf6a80522a8b0c9fa9898fbfaef96
Juvaid <117333674+Juvaid@users.noreply.github.com>
2026-03-13 12:32:15 +0530

    Update index.ts to replace broken imports with static content exports - 2026-03-13 07:01:45

--- data/index.ts
+++ data/index.ts
@@ -1,6 +1,4 @@
-// Central export point for all static content
+// Static content exports
+export const someStaticData = 'This is static content';
 
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as Sidebar } from './Sidebar';
-// Add more exports as needed
+// other exports can be added here...
